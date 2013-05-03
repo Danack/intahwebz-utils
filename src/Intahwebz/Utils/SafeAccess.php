7e8c0e91044a0a2f62a8805c31e5519b4e2ea7b1
Danack <Danack@basereality.com>
2013-05-04 00:05:30 +0100

    Fixed bad Exception.

--- src/Intahwebz/Utils/SafeAccess.php
+++ src/Intahwebz/Utils/SafeAccess.php
@@ -5,10 +5,10 @@ namespace Intahwebz\Utils;
 
 trait SafeAccess {
 	public function __set($name, $value) {
-		throw new Exception("Property [$name] doesn't exist for class [".__CLASS__."] so can't set it");
+		throw new \Exception("Property [$name] doesn't exist for class [".__CLASS__."] so can't set it");
 	}
 	public function __get($name) {
-		throw new Exception("Property [$name] doesn't exist for class [".__CLASS__."] so can't get it");
+		throw new \Exception("Property [$name] doesn't exist for class [".__CLASS__."] so can't get it");
 	}
 }
 
